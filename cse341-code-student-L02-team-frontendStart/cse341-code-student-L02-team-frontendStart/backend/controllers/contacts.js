44adf2c9a246f1e00bc72a63f8d8225d864a1735
angelasmilford <angela.milford123@gmail.com>
2025-09-29 23:14:32 -1100

    Created controllers

---
+++ cse341-code-student-L02-team-frontendStart/cse341-code-student-L02-team-frontendStart/backend/controllers/contacts.js
@@ -0,0 +1,29 @@
+const { ObjectId } = require('mongodb');
+const connectDB = require('../db/connection');
+
+const getAllContacts = async (req, res) => {
+    try {
+        const db = await connectDB();
+        const contacts = await db.collection('contacts').find().toArray();
+        res.json(contacts)
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const getSingleContact = async (req, res) => {
+    try {
+        const db = await connectDB();
+        const contact = await db.collection('contacts').findOne({ _id: new ObjectId(req.paramsid) });
+        
+        if(!contact) {
+            return res.status(404).json({ message: "Contact not found"});
+        }
+
+        res.json(contact);
+    } catch (err) {
+        res.status(500).json({ message: err.message});
+    }
+};
+
+module.exports = { getAllContacts, getSingleContact}
