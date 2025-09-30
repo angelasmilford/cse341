3916e184df55d6fd8722cb0fce5aee6ff6eba53e
angelasmilford <angela.milford123@gmail.com>
2025-09-29 23:24:44 -1100

    Made a correction with the "getSingleContact()" function

--- cse341-code-student-L02-team-frontendStart/cse341-code-student-L02-team-frontendStart/backend/controllers/contacts.js
+++ cse341-code-student-L02-team-frontendStart/cse341-code-student-L02-team-frontendStart/backend/controllers/contacts.js
@@ -14,15 +14,15 @@ const getAllContacts = async (req, res) => {
 const getSingleContact = async (req, res) => {
     try {
         const db = await connectDB();
-        const contact = await db.collection('contacts').findOne({ _id: new ObjectId(req.paramsid) });
+        const contact = await db.collection('contacts').findOne({ _id: new ObjectId(req.params.id) });
         
-        if(!contact) {
-            return res.status(404).json({ message: "Contact not found"});
+        if (!contact) {
+            return res.status(404).json({ message: "Contact not found" });
         }
 
         res.json(contact);
     } catch (err) {
-        res.status(500).json({ message: err.message});
+        res.status(500).json({ message: err.message });
     }
 };
 
